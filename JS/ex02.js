34ea8cdf6ea1401c3e4d36d1c3c1278958b4b784
Feupir <Feupir25@gmail.com>
2025-08-13 14:54:15 -0300

    prompts da idade e nome

--- JS/ex02.js
+++ JS/ex02.js
@@ -6,5 +6,10 @@ frase = "Aqui temos a mesma string, alterada"
 alert(frase);
 
 // fazendo entrada de dados para uma variavel
-frase = prompt("Digite uma frase") 
-alert(frase);
+var nome = prompt("Digite seu nome:")
+alert("Ola "+nome)
+
+// Pegue a idade, exiba a idade
+
+var idade = prompt("Digite sua idade:")
+alert("Sua idade é: "+idade)
