e0107483cda442c7639ac16a7ab495d2bcae0958
Feupir <Feupir25@gmail.com>
2025-08-13 14:47:41 -0300

    todo o ex02 e js ex02.js

---
+++ JS/ex02.js
@@ -0,0 +1,10 @@
+// Tipos de variáveis em JavaScript
+
+var frase = "Aqui temos uma string"
+alert(frase)
+frase = "Aqui temos a mesma string, alterada"
+alert(frase);
+
+// fazendo entrada de dados para uma variavel
+frase = prompt("Digite uma frase") 
+alert(frase);
