2365e47e85895193dbcaf075ca233f884f04298c
Feupir <Feupir25@gmail.com>
2025-08-13 15:03:21 -0300

    Adicionado operações aritmeticas

--- JS/ex02.js
+++ JS/ex02.js
@@ -13,3 +13,18 @@ alert("Ola "+nome)
 
 var idade = prompt("Digite sua idade:")
 alert("Sua idade é: "+idade)
+
+// Operações aritméticas
+var n1, n1, r
+n1 = 14
+n2 = 20
+r = n1 + n2
+alert("A soma de " + n1 + " e " + n2 + " é: " + r)
+
+// considere que os valores n1 e n2 sejam digitados pelo usuário
+
+n1 = parseInt(prompt("Digite o primeiro número:"))
+n2 = parseInt(prompt("Digite o segundo número:"))
+
+r = n1 + n2
+alert("A soma de " + n1 + " e " + n2 + " é: " + r)
