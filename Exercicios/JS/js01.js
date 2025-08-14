7b8cec9ee0af97d5c3349b20e8191d357f0f4dc1
Feupir <Feupir25@gmail.com>
2025-08-14 08:23:26 -0300

    corrigido erro no ex1

--- Exercicios/JS/js01.js
+++ Exercicios/JS/js01.js
@@ -2,5 +2,5 @@ var n1,n2,r
 
 n1=parseFloat(prompt("Digite o primeiro numero: "))
 n2=parseFloat(prompt("Digite o segundo numero: "))
-r=n1+n2
-alert("O resultado da soma é: "+r)
+r=n1-n2
+alert("O resultado da substração é: "+r)
